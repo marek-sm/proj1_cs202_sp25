addcc83a76629f24e1336fc7a81a57bed4576481
Marek Seablom-Michel <marek.seablommichel@gmail.com>
2026-04-18 19:18:32 -0700

    Task 2 Complete

--- proj1.py
+++ proj1.py
@@ -19,3 +19,21 @@ class RegionCondition:
     year: int
     pop: int
     ghg_rate: float
+
+nyc_globerect: GlobeRect = GlobeRect(40.5, 41.0, -74.3, -73.4)
+nyc_region: Region = Region(nyc_globerect, "New York City", "other")
+nyc_region_condition: RegionCondition = RegionCondition(nyc_region, 2020, 20_100_000, 200_000_000.0)
+
+joburg_globerect: GlobeRect = GlobeRect(-26.4, -26.0, 27.8, 28.2)
+joburg_region: Region = Region(joburg_globerect, "Johannesburg", "other")
+joburg_region_condition: RegionCondition = RegionCondition(joburg_region, 2015, 6_000_000, 30_000_000.0)
+
+gulf_of_mexico_globerect: GlobeRect = GlobeRect(18.0, 31.0, -98.0, -81.0)
+gulf_of_mexico_region: Region = Region(gulf_of_mexico_globerect, "Gulf of Mexico", "ocean")
+gulf_of_mexico_region_condition: RegionCondition = RegionCondition(gulf_of_mexico_region, 2026, 0, 0.0)
+
+cal_poly_globerect: GlobeRect = GlobeRect(35.0, 35.5, -121.0, -120.3)
+cal_poly_region: Region = Region(cal_poly_globerect, "Cal Poly", "other")
+cal_poly_region_condition: RegionCondition = RegionCondition(cal_poly_region, 2020, 55_000, 400_000.0)
+
+region_conditions: list[RegionCondition] = [nyc_region_condition, joburg_region_condition, gulf_of_mexico_region_condition, cal_poly_region_condition]
